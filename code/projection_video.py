3fbf30d43ccaa6c03f941753ff542e69f98579f7
Harman Pelia <pelia@usc.edu>
2023-07-17 08:48:59 -0400

    3rd Commit

--- code/projection_video.py
+++ code/projection_video.py
@@ -10,3 +10,16 @@ parser.add_argument('--video', help='Path to the video to be projected',
 args = parser.parse_args()
 
 video_path=args.video
+
+stream = cv2.VideoCapture(0)
+video = cv2.VideoCapture(video_path)
+dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+parameters =  cv2.aruco.DetectorParameters_create()
+
+def find_and_warp(fram, source, cornerIds, dictionary,parameters):
+    (imgH, imgW) = frame.shape[:2]
+    (srcH, srcW) = source.shape[:2]
+    markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(frame, dictionary, parameters=parameters)
+    if len(markerCorners) != 4:
+        markerIds = np.array([])
+        
